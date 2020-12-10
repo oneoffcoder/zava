d7b4bf95e42de2fa11280419fb513e3394b9ade6
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-10 16:34:02 -0500

    test rotation matrix generator

--- zava-ts/projects/zava/src/lib/zava.core.spec.ts
+++ zava-ts/projects/zava/src/lib/zava.core.spec.ts
@@ -1,10 +1,90 @@
 import { TestBed } from '@angular/core/testing';
 
-import { GrandTour } from './zava.core';
+import { Util } from './zava.core';
 
 describe('ZavaCore', () => {
 
-  it('should do nothing', () => {
-    const grandTour = new GrandTour();
+  it('should create a 3 x 3 square rotation matrix', () => {
+    const n = 3;
+    const i = 0;
+    const j = 1;
+    const deg = 1.0;
+    const sin = Math.sin(deg);
+    const cos = Math.cos(deg);
+    const matrix = Util.getRotationMatrix(n, sin, cos, i, j);
+
+    expect(matrix.length).toBe(3);
+    [...matrix].forEach(row => expect(row.length).toBe(3));
+    expect(matrix[i][i]).toBe(cos);
+    expect(matrix[j][j]).toBe(cos);
+    expect(matrix[i][j]).toBe(sin);
+    expect(matrix[j][i]).toBe(-sin);
+    for (let r = 0; r < n; r++) {
+      for (let c = 0; c < n; c++) {
+        if ((r === i && c === i) || (r === j && c === j) || (r === i && c === j) || (r === j && c === i)) {
+          continue;
+        } else if (r === c) {
+          expect(matrix[r][c]).toBe(1.0);
+        } else {
+          expect(matrix[r][c]).toBe(0.0);
+        }
+      }
+    }
+  });
+
+  it('should create a 4 x 4 square rotation matrix', () => {
+    const n = 4;
+    const i = 0;
+    const j = 1;
+    const deg = 1.0;
+    const sin = Math.sin(deg);
+    const cos = Math.cos(deg);
+    const matrix = Util.getRotationMatrix(n, sin, cos, i, j);
+
+    expect(matrix.length).toBe(n);
+    [...matrix].forEach(row => expect(row.length).toBe(n));
+    expect(matrix[i][i]).toBe(cos);
+    expect(matrix[j][j]).toBe(cos);
+    expect(matrix[i][j]).toBe(sin);
+    expect(matrix[j][i]).toBe(-sin);
+    for (let r = 0; r < n; r++) {
+      for (let c = 0; c < n; c++) {
+        if ((r === i && c === i) || (r === j && c === j) || (r === i && c === j) || (r === j && c === i)) {
+          continue;
+        } else if (r === c) {
+          expect(matrix[r][c]).toBe(1.0);
+        } else {
+          expect(matrix[r][c]).toBe(0.0);
+        }
+      }
+    }
+  });
+
+  it('should create a 10 x 10 square rotation matrix', () => {
+    const n = 10;
+    const i = 0;
+    const j = 1;
+    const deg = 1.0;
+    const sin = Math.sin(deg);
+    const cos = Math.cos(deg);
+    const matrix = Util.getRotationMatrix(n, sin, cos, i, j);
+
+    expect(matrix.length).toBe(n);
+    [...matrix].forEach(row => expect(row.length).toBe(n));
+    expect(matrix[i][i]).toBe(cos);
+    expect(matrix[j][j]).toBe(cos);
+    expect(matrix[i][j]).toBe(sin);
+    expect(matrix[j][i]).toBe(-sin);
+    for (let r = 0; r < n; r++) {
+      for (let c = 0; c < n; c++) {
+        if ((r === i && c === i) || (r === j && c === j) || (r === i && c === j) || (r === j && c === i)) {
+          continue;
+        } else if (r === c) {
+          expect(matrix[r][c]).toBe(1.0);
+        } else {
+          expect(matrix[r][c]).toBe(0.0);
+        }
+      }
+    }
   });
 });
--- zava-ts/projects/zava/src/lib/zava.core.ts
+++ zava-ts/projects/zava/src/lib/zava.core.ts
@@ -2,3 +2,31 @@ export class GrandTour {
   constructor() {
   }
 }
+
+
+export class Util {
+  private constructor() {
+  }
+
+  static getRotationMatrix(n: number, sin: number, cos: number, i: number, j: number): Array<Array<number>> {
+    const matrix = new Array<Array<number>>();
+    for (let r = 0; r < n; r++) {
+      const row = new Array<number>();
+      for (let c = 0; c < n; c++) {
+        if ((r === i && c === i) || (r === j && c === j)) {
+          row.push(cos);
+        } else if (r === i && c === j) {
+          row.push(sin);
+        } else if (r === j && c === i) {
+          row.push(-sin);
+        } else if (r === c) {
+          row.push(1.0);
+        } else {
+          row.push(0.0);
+        }
+      }
+      matrix.push(row);
+    }
+    return matrix;
+  }
+}
