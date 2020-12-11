12e51860dd21a94cdf52e89d6d83370f57c7ae1e
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-11 05:02:33 -0500

    add; https://blog.logrocket.com/data-visualization-angular-d3/

--- vizapp/src/app/app.module.ts
+++ vizapp/src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BarComponent } from './bar/bar.component';
+import { PieComponent } from './pie/pie.component';
+import { ScatterComponent } from './scatter/scatter.component';
+import { PcoordComponent } from './pcoord/pcoord.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarComponent,
+    PieComponent,
+    ScatterComponent,
+    PcoordComponent
   ],
   imports: [
     BrowserModule,
---
+++ vizapp/src/app/pcoord/pcoord.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-pcoord',
+  templateUrl: './pcoord.component.html',
+  styleUrls: ['./pcoord.component.scss']
+})
+export class PcoordComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
