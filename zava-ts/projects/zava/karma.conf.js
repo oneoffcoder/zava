0927391628982b5836126f217a582c2b9f9c7277
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-11 03:58:25 -0500

    clean up

--- zava-ts/projects/zava/karma.conf.js
+++ zava-ts/projects/zava/karma.conf.js
@@ -25,11 +25,13 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, '../../coverage/zava'),
+      dir: require('path').join(__dirname, 'coverage'),
       subdir: '.',
       reporters: [
         { type: 'html' },
-        { type: 'text-summary' }
+        { type: 'text-summary' },
+        { type: 'lcovonly' },
+        { type: 'json' }
       ]
     },
     reporters: ['progress', 'kjhtml'],
--- zava-ts/projects/zava/src/public-api.ts
+++ zava-ts/projects/zava/src/public-api.ts
@@ -2,7 +2,4 @@
  * Public API Surface of zava
  */
 
-export * from './lib/zava.service';
-export * from './lib/zava.component';
-export * from './lib/zava.module';
 export * from './lib/zava.core';
