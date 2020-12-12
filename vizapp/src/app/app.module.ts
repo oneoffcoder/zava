e9e14b615a22b62f935c66d51a34a4c668407e28
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-11 21:48:11 -0500

    add material

--- vizapp/src/app/app.module.ts
+++ vizapp/src/app/app.module.ts
@@ -7,6 +7,8 @@ import { BarComponent } from './bar/bar.component';
 import { PieComponent } from './pie/pie.component';
 import { ScatterComponent } from './scatter/scatter.component';
 import { PcoordComponent } from './pcoord/pcoord.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonModule} from '@angular/material/button';
 
 @NgModule({
   declarations: [
@@ -18,7 +20,9 @@ import { PcoordComponent } from './pcoord/pcoord.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
