f815a8017175db8c861b000eef10bb2b4c1f4ce4
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-11 23:27:25 -0500

    remove

--- vizapp/src/app/app.module.ts
+++ vizapp/src/app/app.module.ts
@@ -3,9 +3,6 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { BarComponent } from './bar/bar.component';
-import { PieComponent } from './pie/pie.component';
-import { ScatterComponent } from './scatter/scatter.component';
 import { PcoordComponent } from './pcoord/pcoord.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonModule} from '@angular/material/button';
@@ -13,9 +10,6 @@ import {MatButtonModule} from '@angular/material/button';
 @NgModule({
   declarations: [
     AppComponent,
-    BarComponent,
-    PieComponent,
-    ScatterComponent,
     PcoordComponent
   ],
   imports: [
