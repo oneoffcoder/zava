dc2d6c3bdd895a6c1dcb08d9a1621a46bce8a9bd
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-13 00:35:49 -0500

    start moving scripts to root dir

--- zava-ts/projects/zava/karma.conf.js
+++ zava-ts/projects/zava/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, 'coverage'),
+      dir: require('path').join(__dirname, '../../coverage/zava'),
       subdir: '.',
       reporters: [
         { type: 'html' },
