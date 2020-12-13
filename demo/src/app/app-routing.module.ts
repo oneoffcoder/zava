54af0de5c03ccbd1667a4b28fbcbec26b28fe13c
Jee Vang, Ph.D <vangjee@gmail.com>
2020-12-13 04:16:49 -0500

    add

--- demo/src/app/app-routing.module.ts
+++ demo/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '**', component: DefaultComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- demo/src/app/app.module.ts
+++ demo/src/app/app.module.ts
@@ -3,14 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DefaultComponent } from './default/default.component';
+import {ZavaModule} from '@oneoffcoder/zava';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ZavaModule
   ],
   providers: [],
   bootstrap: [AppComponent]
